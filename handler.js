1c3bf83f5dde4f5d9c20f65f913b7d11994897e1
Rob Jewell <jewell.robertp@gmail.com>
2022-04-20 11:09:49 -0400

    query dynamo and add error steps

--- handler.js
+++ handler.js
@@ -1,7 +1,44 @@
 'use strict';
+const AWS = require("aws-sdk");
+const DynamoDB = require("aws-sdk/clients/dynamodb");
+const DocumentClient = new DynamoDB.DocumentClient({
+  region: 'us-east-2'
+});
 
-module.exports.checkInventory = async (event) => {
-  return "Stocks available";
+const isBookAvailable = (book, quantity) => {
+  return (book.quantity - quantity) > 0
+}
+
+module.exports.checkInventory = async (bookId, quantity) => {
+  try {
+    let params = {
+      TableName: 'bookTable',
+      KeyConditionExpression: 'bookId = :bookId',
+      ExpressionAttributeValues: {
+        ':bookId': bookId
+      }
+    }
+
+    let result = await DocumentClient.query(params).promise();
+    let book = result.Items[0];
+
+    if (isBookAvailable(book, quantity)) {
+      return book;
+    } else {
+      let bookOutOfStockError = new Error("The book is out of stock")
+      bookOutOfStockError.name = "BookOutOfStock";
+      throw bookOutOfStockError;
+    }
+
+  } catch(error) {
+    if (error.name === 'BookOutOfStock') {
+      throw error;
+    } else {
+      let bookNotFoundError = new Error(error);
+      bookNotFoundError.name = 'BookNotFound';
+      throw bookNotFoundError;
+    }
+  }
 };
 
 module.exports.calculateTotal = async (event) => {
